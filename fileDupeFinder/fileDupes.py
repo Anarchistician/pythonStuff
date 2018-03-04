43d3bf1facc84f2615e94d6cd8f142894ebf868f
Anarchistician <anarchistician@gmail.com>
2018-03-04 08:52:25 -0800

    Added actual script

---
+++ fileDupeFinder/fileDupes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  4 07:28:16 2018
+
+@author: phil
+"""
+
+import os
+import pandas as pd
+import re
+
+def fileDump(rootDir):
+    allFiles = []
+    allDirs = []
+    
+    for e in os.walk(rootDir):
+        #   Remove hidden files and folders
+        tFiles = [a for a in e[2] if not re.match(r'^\..+',a)]
+        allFiles += tFiles
+        allDirs += [e[0]] * len(tFiles)
+    #   Print dimensions... Originally for debugging purposes, but I left it.
+    print(len(allFiles),len(allDirs))
+    #   Return in a data frame, because it's easier to csv it.
+    fileList = pd.DataFrame({'file': allFiles, 'dir': allDirs})
+    fileList = fileList.sort_values(by = 'dir')
+    return fileList
+
+def printDupes(rootDir):
+    fileList = fileDump(rootDir)
+    dupes = fileList.duplicated(subset='dir',keep=False)
+    dupes.to_csv('dupedFiles.csv')
+
+
